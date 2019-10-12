c88b300075014bddf61ab44c607c4ba414db64c2
Tam Vu <vutam_a@hotmail.com>
2019-10-12 22:26:18 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,7 +4,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change 
     create_table :haunted_houses do |t|
       t.string :name
-      t.
+      t.string :location
+      
     end 
   end 
   
